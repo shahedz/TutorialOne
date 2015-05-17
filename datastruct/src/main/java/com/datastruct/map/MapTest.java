29f14922659279d8066bccf8a795998b4a2a2e7b
shahedz <shahed.zyman@medialets.com>
2015-05-16 22:45:52 -0400

    aber mvc

---
+++ datastruct/src/main/java/com/datastruct/map/MapTest.java
@@ -0,0 +1,56 @@
+package com.datastruct.map;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class MapTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+
+
+		String[] keyValue = {"1", "2", "3", "4", "5", "6"};
+		
+		Character ch = new Character('a');
+		
+		List<String> test = new ArrayList<String>();
+		String[] isPlandrome = {"ABAB"};
+		
+		String x ="abab";
+		char[] c = x.toCharArray();
+		for(char d: c){
+		
+		System.out.println("new: "+ c);
+		
+		}
+
+		
+//		for(String c: isPlandrome){
+//			System.out.println(c);
+//			
+//			
+//		}
+		
+	}
+
+	
+//	
+//	public void test(){
+//	// Given the following map
+//	Map<KeyClass, ValueClass> myMap;
+//
+//	// Iterate all keys
+//	for (KeyClass key  : myMap.keySet()) 
+//	    System.out.println(key);
+//
+//	// Iterate all values
+//	for (ValueClass value  : myMap.values()) 
+//	    System.out.println(value);
+//
+//	// Iterate all key/value pairs
+//	for (Entry<KeyClass, ValueClass> entry  : myMap.entrySet()) 
+//	    System.out.println(entry.getKey() + " - " + entry.getValue());
+//	}
+}
