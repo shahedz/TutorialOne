3e0469797a696faee5df8c764a86df94c57db0a2
shahedz <shahed.zyman@medialets.com>
2015-06-28 11:40:03 -0400

    New Project

---
+++ ArrayProject/src/com/array/framework/ArrayClass.java
@@ -0,0 +1,24 @@
+package com.array.framework;
+
+public class ArrayClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int[] x = new int[2];
+		
+		x[1] = 2;
+		
+		System.out.println(x[0]);
+		
+		
+		
+		int i[] = {1,2,3};
+
+		
+		System.out.println(i[2]);
+
+	}
+
+}
